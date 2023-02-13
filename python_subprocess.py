38ed317557ec346acdd54c3d15c9ad7cd45b87d9
sitthatkaja <sitthatkaja@kkumail.com>
2023-02-13 22:15:04 +0700

    Test HW

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,13 +2,18 @@ import subprocess #สำหรับ รัน terminal command
 
 if __name__ == "__main__":
     ## basic
-    subprocess.run(["ls","-ltr"])
-    subprocess.run(["python","python_script101.py","--x","8"])
+    # subprocess.run(["ls","-ltr"])
+    # subprocess.run(["python","python_script101.py","--x","8"])
 
     ## use output of subprocess
-    process1 = subprocess.Popen(["python","python_script101.py","--x","8"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = process1.communicate()
-    print(out.decode('utf-8'))
+    # process1 = subprocess.Popen(["python","python_script101.py","--x","6"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # out, err = process1.communicate()
+    # print(out.decode('utf-8'))
 
+    for x in range(1,10):
+        print(f'{x}')
+        process = subprocess.Popen(["python","python_script101.py","--x"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        print(out.decode('utf-8'))
 
 #สั่งรัน python_script101.py 50 รอบ โดย x = 1,3,5,7.... แล้วให้เอา output ของ xt มารวมกันแล้วปริ้นออกมา
